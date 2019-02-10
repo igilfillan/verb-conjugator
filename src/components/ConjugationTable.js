d5fb5f0633f7cda37c9a02065303aa35613558b8
IGilfillan <igwebconsulting@gmail.com>
2019-02-10 22:19:15 +0000

    Add ConjugationTable component

--- src/components/ConjugationTable.js
+++ src/components/ConjugationTable.js
@@ -1,5 +1,89 @@
 import React from 'react';
 
-const ConjugationTable = data => {};
+const ConjugationTable = ( {data} ) => {
+
+   const getTense = (tense) => {
+        return data.find( (obj) => {
+            return obj.tense === tense;
+        });
+    };
+
+   console.log('data', data);
+    const presente = getTense('Presente');
+    const preterito = getTense('Pretérito');
+    const imperfecto = getTense('Imperfecto');
+    const condicional = getTense('Condicional');
+    const futuro = getTense('Futuro');
+
+    return (
+            <table>
+                <thead>
+                <tr>
+                    <th>
+                        <h2>Present</h2>
+                    </th>
+                    <th>
+                        <h2>Preterito</h2>
+                    </th>
+                    <th>
+                        <h2>Imperfecto</h2>
+                    </th>
+                    <th>
+                        <h2>Condicional</h2>
+                    </th>
+                    <th>
+                        <h2>Futuro</h2>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>{presente.form_1s}</td>
+                    <td>{preterito.form_1s} </td>
+                    <td>{imperfecto.form_1s} </td>
+                    <td>{condicional.form_1s} </td>
+                    <td>{futuro.form_1s} </td>
+                </tr>
+                <tr>
+                    <td>{presente.form_2s} </td>
+                    <td>{preterito.form_2s} </td>
+                    <td>{imperfecto.form_2s} </td>
+                    <td>{condicional.form_2s} </td>
+                    <td>{futuro.form_2s} </td>
+                </tr>
+                <tr>
+                    <td>{presente.form_3s} </td>
+                    <td>{preterito.form_3s} </td>
+                    <td>{imperfecto.form_3s} </td>
+                    <td>{condicional.form_3s} </td>
+                    <td>{futuro.form_3s} </td>
+                </tr>
+                <tr>
+                    <td>{presente.form_1p} </td>
+                    <td>{preterito.form_1p} </td>
+                    <td>{imperfecto.form_1p} </td>
+                    <td>{condicional.form_1p} </td>
+                    <td>{futuro.form_1p} </td>
+                </tr>
+                <tr>
+                    <td>{presente.form_2p} </td>
+                    <td>{preterito.form_2p} </td>
+                    <td>{imperfecto.form_2p} </td>
+                    <td>{condicional.form_2p} </td>
+                    <td>{futuro.form_2p} </td>
+                </tr>
+                <tr>
+                    <td>{presente.form_3p} </td>
+                    <td>{preterito.form_3p} </td>
+                    <td>{imperfecto.form_3p} </td>
+                    <td>{condicional.form_3p} </td>
+                    <td>{futuro.form_3p} </td>
+                </tr>
+                </tbody>
+            </table>
+
+    )
+
+};
 
 export default ConjugationTable;
--- src/pages/Verb.js
+++ src/pages/Verb.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from '../components/Navigation';
+import ConjugationTable from '../components/ConjugationTable';
 import verbData from '../verb-list-spanish.json';
 
 
@@ -9,6 +10,7 @@ class Verb extends Component {
     super(props);
 
     const currentVerb = this.props.match.params.infinitive;
+
     let verbEntries = verbData.filter( entry => {
       return entry.infinitive === currentVerb;
     });
@@ -16,127 +18,34 @@ class Verb extends Component {
     this.state = { verbEntries: verbEntries };
   }
 
-
-  getTense(mood, tense){
-
-    let verbTense = this.state.verbEntries.find( (obj) => {
-      return obj.mood === mood && obj.tense === tense;
+  getMood(mood){
+    return this.state.verbEntries.filter( (item) => {
+        return item.mood === mood;
     });
-
-    console.log('verbTense', verbTense);
-    return verbTense;
-
   }
-
-  normaliseVerbItems = item => ({
-    [item.tense.toLowerCase()]: {
-        "form_1s": item.form_1s,
-        "form_2s": item.form_2s,
-        "form_3s": item.form_3s,
-        "form_1p": item.form_1p,
-        "form_2p": item.form_2p,
-        "form_3p": item.form_3p
-    }
-  });
-
-  getIndiciative = () => {
-
-    const transVerb = this.state.verbEntries.map(this.normaliseVerbItems);
-
-      // let flatVerbs = transVerb.reduce((obj, item) => {
-      //   obj[item[0]] = item
-      //   return obj
-      // }, {})
-
-    console.log('transVerb', transVerb);
-    // console.log('flatVerbs', flatVerbs);
-
-    // return {
-    //   "mood": "Indicativo",
-    //   "mood_english": "Indicative",
-    //   "tense": {
-    //     "presente": {
-    //       "form_1s": "abandono",
-    //       "form_2s": "abandonas",
-    //       "form_3s": "abandona",
-    //       "form_1p": "abandonamos",
-    //       "form_2p": "abandonáis",
-    //       "form_3p": "abandonan",
-    //       "gerund": "abandonando"
-    //     },
-    //     "futuro": {
-    //       "form_1s": "abandono",
-    //       "form_2s": "abandonas",
-    //       "form_3s": "abandona",
-    //       "form_1p": "abandonamos",
-    //       "form_2p": "abandonáis",
-    //       "form_3p": "abandonan",
-    //       "gerund": "abandonando"
-    //     }
-    //
-    //   }
-    // }
-
-  }
-
 
   render(){
 
     const { match } = this.props;
 
-    const indicativePresente = this.getTense('Indicativo', 'Presente');
-    const indicativePreterito = this.getTense('Indicativo', 'Pretérito');
-    const indicativeImperfecto = this.getTense('Indicativo', 'Imperfecto');
+    console.log('indicative', this.getMood('Indicativo'));
 
-    this.getIndiciative();
 
-    return (
+      return (
         <>
           <Navigation />
+
           <header className="App-header">
             {match.params.infinitive}
           </header>
 
-          <table>
-            <thead>
-            <tr>
-            <th>
-              <h2>Present Tense</h2></th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <td>{indicativePresente.form_1s} </td>
-              <td>{indicativePreterito.form_1s} </td>
-              <td>{indicativeImperfecto.form_1s} </td>
-            </tr>
-            <tr>
-              <td>{indicativePresente.form_2s} </td>
-              <td>{indicativePreterito.form_2s} </td>
-              <td>{indicativeImperfecto.form_2s} </td>
-            </tr>
-            <tr>
-              <td>{indicativePresente.form_3s} </td>
-              <td>{indicativePreterito.form_3s} </td>
-              <td>{indicativeImperfecto.form_3s} </td>
-            </tr>
-            <tr>
-              <td>{indicativePresente.form_1p} </td>
-              <td>{indicativePreterito.form_1p} </td>
-              <td>{indicativeImperfecto.form_1p} </td>
-            </tr>
-            <tr>
-              <td>{indicativePresente.form_2p} </td>
-              <td>{indicativePreterito.form_2p} </td>
-              <td>{indicativeImperfecto.form_2p} </td>
-            </tr>
-            <tr>
-              <td>{indicativePresente.form_3p} </td>
-              <td>{indicativePreterito.form_3p} </td>
-              <td>{indicativeImperfecto.form_3p} </td>
-            </tr>
-            </tbody>
-          </table>
+          <p>{this.state.verbEntries[0].infinitive_english}</p>
+
+            <h2>Indicativo</h2>
+          <ConjugationTable data={this.getMood('Indicativo')} />
+
+            <h2>Subjuntivo</h2>
+          {/*<ConjugationTable data={this.getMood('Subjuntivo')} />*/}
 
         </>
     )
@@ -146,3 +55,4 @@ class Verb extends Component {
 };
 
 export default Verb;
+
