435040dc4dd5ae15dbbfad764f4c6a002dba79d5
igilfill <iain_gilfillan@discovery.com>
2019-08-07 16:31:34 +0100

    Adds methods and tests to latinize text strings

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+const latinize = string => {
+  return string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+};
+
+const isLatinMatch = (userStr, actualStr) => {
+  return latinize(userStr).toLowerCase() === latinize(actualStr).toLowerCase();
+};
+const getMatch = (userString, actualString) => {
+  let match =
+    userString === actualString
+      ? "full"
+      : isLatinMatch(userString, actualString)
+      ? "partial"
+      : "none";
+  return {
+    type: match
+  };
+};
+
+it("removes diacritics from a string", () => {
+  expect(latinize("áÁéÉíÍóÓúÚñÑüÜ")).toEqual("aAeEiIoOuUnNuU");
+  expect(latinize("áéíol")).toEqual("aeiol");
+});
+
+it("compares strings with diacritics removed", () => {
+  expect(isLatinMatch("áéíól", "aeiól")).toEqual(true);
+  expect(isLatinMatch("áéí ól", "aeiól")).toEqual(false);
+  expect(isLatinMatch("aaeeIIooUunNuu", "áÁéÉíÍóÓúÚñÑüÜ")).toEqual(true);
+});
+
+it("", () => {
+  expect(latinize("áÁéÉíÍóÓúÚñÑüÜ")).toEqual("aAeEiIoOuUnNuU");
+  expect(latinize("áéíol")).toEqual("aeiol");
 });
